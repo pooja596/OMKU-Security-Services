6c63296b5aecb179ffaf6bc0839c082314895206
pooja <pooja@LAPTOP-5PMN6B7E>
2020-04-26 15:34:21 +0530

    Initial Release with CR and defect fixes

--- OMKU Security/WebContent/assets/js/main.js
+++ OMKU Security/WebContent/assets/js/main.js
@@ -129,24 +129,6 @@
     $('.venobox').venobox();
   });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: {
-      0: {
-        items: 1
-      },
-      768: {
-        items: 1
-      },
-      900: {
-        items: 2
-      }
-    }
-  });
-
   // Initi AOS
   AOS.init({
     duration: 800,
